8da764cbeabec300a7921a065a5c969d6531caba
juanbarbecho <juan.bar310@gmail.com>
2024-09-25 22:09:33 -0400

    Create display.h

---
+++ display.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void open_display(void);
+void close_display(void);
+void display_letter(int letter, int xOffset, int yOffset);
+void clear_display(void);
+void open_input(void);
+int check_input(int delay);
